51d70d8afc6c7720abbfbc205022dfbf27016be0
spindly <kaihrmt@gmail.com>
2025-10-22 14:34:13 +0100

    Added tournament selection

--- ga/selection.py
+++ ga/selection.py
@@ -1,12 +1,12 @@
-from custom_types import Chromosome, Individual
+from custom_types import Population
 import random
 
 def roulette():
     pass
 
-def tournament(population: list[Individual],fitness: list[float]) -> Individual:
-    selected_parents = []
-    pop_size = len(population)
+def tournament(population: Population,fitness: list[float]) -> Population:
+    selected_parents: Population = []
+    pop_size:int = len(population)
     for _ in range(pop_size):
         p1,p2 = (random.randint(0,pop_size) for _ in range(2))
         if fitness[p1] > fitness[p2]:
