db2dd61b3760b643a855dfa1b2b02bc9f0ee3e27
spindly <kaihrmt@gmail.com>
2025-10-25 21:09:09 +0100

    updating target sol to include tibia and femur

--- ga/target_sol.py
+++ ga/target_sol.py
@@ -1,8 +1,8 @@
 import math
 import output
-def produce_target(gait):
+def produce_target(gait_length:int):
     best = []
-    for idx in range(gait):
+    for idx in range(gait_length):
         frame = []
         for joint in range(24):
             opposite = (joint) % 2 == 0
@@ -10,12 +10,27 @@ def produce_target(gait):
                 opposite = not opposite
             if joint % 3 ==0:
                 # idx = (-idx) if opposite else idx
-                target = (30*(math.sin(0.5*idx)))
+                target = (23*(math.sin(0.4*idx)))
                 target = (-target) if opposite else target
                 frame.append(target)
+            # elif joint % 2 == 0:
+                # joint is femur
+                # frame.append(-10)
             else:
-                frame.append(0)
-            pass
+                # idx = idx if opposite else (-idx)
+                # target = ((50)*(math.sin(0.4*idx)))
+                # target = (100*(math.sin(0.4*(-idx)))) -100
+                # 0.2 for peroid looks good, but doesnt do it enough sometimes
+                target = (100*(math.sin(0.4*idx+2))) -100
+                # target = -(target)
+                if target <= -50:
+                    target = -50
+                # target = (-target) if opposite else target
+                # target = target if opposite else (-target)
+
+                frame.append(target)
+                # need to check if for leg down we want it to stay at 50 and then go down to 0 when up
+                # frame.append(-50)
         best.append(frame)
     print(best)
     output.output("sol.txt",best)
