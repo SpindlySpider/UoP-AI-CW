f1380c9478de96c7508c1e3f1b0c2871a53c2a0e
spindly <kaihrmt@gmail.com>
2025-10-28 22:08:46 +0100

    backup of comments

--- ga/fitness.py
+++ ga/fitness.py
@@ -17,7 +17,12 @@ class fitness():
         fit_dict:dict[str,float] = {"coxa":0,"femur":0,"tibia":0}
         # used to get current joint in loop
         joint_names = ["coxa","femur","tibia"]
-        gait = self.gen_gait(individual)
+        gait = gen_gait(individual,self.gait_length)
+        print(gait[0])
+        print(len(gait[0]))
+        print(gait[0][0],gait[0][3],gait[0][6],gait[0][9])
+        print(gait[0][12],gait[0][15],gait[0][18],gait[0][21])
+        print(len(gait[0]))
 
         for frame_idx in range(self.gait_length):
             for chromosome_idx in range(24):
@@ -44,15 +49,33 @@ class fitness():
         fit_val = fit_dict["coxa"] + fit_dict["femur"] + fit_dict["tibia"]
         return fit_val
 
-    def gen_gait(self,individual:Individual) -> Gait:
-        gait:Gait = []
-        # need to gen the length of the gait using sin params in f
-        for idx in range(self.gait_length):
-            gait.append([])
-            for chromosome in individual:
-                mag, period, offset, neg = chromosome
-                sin_val:float = (period*idx)+offset
-                sin_val:float = (-sin_val) if neg else sin_val
-                predict:float = mag*math.sin(sin_val)
-                gait[idx].append(predict)
-        return gait
+def gen_gait(individual:Individual,gait_length:int) -> Gait:
+    gait:Gait = []
+    # need to gen the length of the gait using sin params in f
+    for idx in range(gait_length):
+        gait.append([])
+        # limb_counter = 0
+        prev_limb = []
+        for c_idx,chromosome in enumerate(individual):
+            mag, period, offset, neg = chromosome
+            sin_val:float = (period*idx)+offset
+            sin_val:float = (-sin_val) if neg else sin_val
+            predict:float = mag*math.sin(sin_val)
+            predict = predict if predict > -50 else -50
+            # if c_idx // 3 != limb_counter and c_idx % 3 == 2 and c_idx % 3 > 0 and len(prev_limb) >= 3:
+            # if limb_counter  > 1 and c_idx % 3 == 0 and limb_counter != c_idx // 3:
+            # if len(gait[idx]) // 3 > limb_counter and c_idx // 3 > 1:
+            # wait if gets to 2 limbs then pop stack
+            prev_limb.append(predict)
+            gait[idx].append(predict)
+            # limb_counter += 1
+            if len(prev_limb) == 6:
+                # repeat prev two
+                gait[idx] = gait[idx] + prev_limb[:3] + prev_limb[3:]
+                prev_limb = []
+        # gait[idx] = gait[idx] + prev_limb[:3]
+        # print("prev",len(prev_limb))
+        # prev_limb = prev_limb[3:]
+        # gait[idx] = gait[idx] + prev_limb[:3]
+
+    return gait
