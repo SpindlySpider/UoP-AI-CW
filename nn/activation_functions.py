8026f7e623f4b099a02e46f6523f3a08ed9cb8ae
spindly <kaihrmt@gmail.com>
2025-11-11 20:01:48 +0000

    Remove lambda bc pickle
    Had to remove lambda as pickle cannot serialize annoymous lambda
    functions

--- nn/activation_functions.py
+++ nn/activation_functions.py
@@ -1,13 +1,14 @@
 import numpy as np
 
-sigmoid = lambda x: 1/(1+np.exp(-x))
-sigmoid_derivitive = lambda x: x*(1-x)
+# not using lambda functions here as pickle cannot serialize anonymous functions
+def sigmoid(x): return 1/(1+np.exp(-x))
+def sigmoid_derivitive(x):return x*(1-x)
 
-linear = lambda x : x
-linear_derivative = lambda x: 1
+def linear(x): return x
+def linear_derivative(x): return 1
 
-tanh = lambda x : (np.exp(2*x)-1) / (np.exp(2*x) + 1)
-tanh_derititive = lambda x : 1 - (tanh(x)**2)
+def tanh(x): return (np.exp(2*x)-1) / (np.exp(2*x) + 1)
+def tanh_derititive(x): return 1 - (tanh(x)**2)
 
 # could add ReLu here and see how it does :) 
 
